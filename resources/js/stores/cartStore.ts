4663faaca1137555ca2ca30a6e76f7169d746389
preslavstoyanov <preslav.stoyanov@abv.bg>
2024-09-16 22:52:40 +0300

    persistent cart store data

--- resources/js/stores/cartStore.ts
+++ resources/js/stores/cartStore.ts
@@ -1,5 +1,6 @@
 import { CartPizza } from "@/types";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type CartState = {
     pizzas: CartPizza[];
@@ -14,82 +15,91 @@ type CartState = {
     removePizza: (pizzaIndex: number) => void;
 };
 
-export const useCartStore = create<CartState>()((set) => ({
-    pizzas: [],
-    increaseQuantity: (pizzaIndex) =>
-        set((state) => {
-            const pizzas = state.pizzas;
-            const pizza = pizzas[pizzaIndex];
-            if (pizza && pizza.quantity < 32) pizza.quantity++;
+export const useCartStore = create<CartState>()(
+    persist(
+        (set) => ({
+            pizzas: [],
+            increaseQuantity: (pizzaIndex) =>
+                set((state) => {
+                    const pizzas = state.pizzas;
+                    const pizza = pizzas[pizzaIndex];
+                    if (pizza && pizza.quantity < 32) pizza.quantity++;
 
-            return {
-                pizzas: [...pizzas],
-            };
+                    return {
+                        pizzas: [...pizzas],
+                    };
+                }),
+
+            decreaseQuantity: (pizzaIndex) =>
+                set((state) => {
+                    const pizzas = state.pizzas;
+                    const pizza = pizzas[pizzaIndex];
+                    if (pizza && pizza.quantity > 1) pizza.quantity--;
+
+                    return {
+                        pizzas: [...pizzas],
+                    };
+                }),
+
+            removeWithoutIngredient: (pizzaIndex, ingredient) =>
+                set((state) => {
+                    const pizzas = state.pizzas;
+                    const pizza = pizzas[pizzaIndex];
+                    if (pizza && pizza.withoutIngredients) {
+                        pizza.withoutIngredients =
+                            pizza.withoutIngredients.filter(
+                                (i) => i !== ingredient,
+                            );
+                    }
+
+                    return {
+                        pizzas: [...pizzas],
+                    };
+                }),
+
+            setWithoutIngredients: (pizzaIndex, withoutIngredients) =>
+                set((state) => {
+                    const pizzas = state.pizzas;
+                    const pizza = pizzas[pizzaIndex];
+                    if (pizza) {
+                        pizza.withoutIngredients = pizza.ingredients.filter(
+                            (ingredient) =>
+                                withoutIngredients.includes(ingredient),
+                        );
+                    }
+
+                    return {
+                        pizzas: [...pizzas],
+                    };
+                }),
+
+            addPizza: (pizza) =>
+                set((state) => {
+                    const pizzas = state.pizzas;
+                    const newPizza: CartPizza = {
+                        id: pizzas.length,
+                        ...pizza,
+                    };
+
+                    pizzas.push(newPizza);
+
+                    return {
+                        pizzas: [...pizzas],
+                    };
+                }),
+
+            removePizza: (pizzaIndex) =>
+                set((state) => {
+                    const pizzas = state.pizzas;
+                    pizzas.splice(pizzaIndex, 1);
+
+                    return {
+                        pizzas: [...pizzas],
+                    };
+                }),
         }),
-
-    decreaseQuantity: (pizzaIndex) =>
-        set((state) => {
-            const pizzas = state.pizzas;
-            const pizza = pizzas[pizzaIndex];
-            if (pizza && pizza.quantity > 1) pizza.quantity--;
-
-            return {
-                pizzas: [...pizzas],
-            };
-        }),
-
-    removeWithoutIngredient: (pizzaIndex, ingredient) =>
-        set((state) => {
-            const pizzas = state.pizzas;
-            const pizza = pizzas[pizzaIndex];
-            if (pizza && pizza.withoutIngredients) {
-                pizza.withoutIngredients = pizza.withoutIngredients.filter(
-                    (i) => i !== ingredient,
-                );
-            }
-
-            return {
-                pizzas: [...pizzas],
-            };
-        }),
-
-    setWithoutIngredients: (pizzaIndex, withoutIngredients) =>
-        set((state) => {
-            const pizzas = state.pizzas;
-            const pizza = pizzas[pizzaIndex];
-            if (pizza) {
-                pizza.withoutIngredients = pizza.ingredients.filter(
-                    (ingredient) => withoutIngredients.includes(ingredient),
-                );
-            }
-
-            return {
-                pizzas: [...pizzas],
-            };
-        }),
-
-    addPizza: (pizza) =>
-        set((state) => {
-            const pizzas = state.pizzas;
-            const newPizza: CartPizza = {
-                id: pizzas.length,
-                ...pizza,
-            };
-
-            pizzas.push(newPizza);
-
-            return {
-                pizzas: [...pizzas],
-            };
-        }),
-
-    removePizza: (pizzaIndex) =>
-        set((state) => {
-            const pizzas = state.pizzas;
-            pizzas.splice(pizzaIndex, 1);
-
-            return {
-                pizzas: [...pizzas],
-            };
-        }),
-}));
+        {
+            name: "cart-storage",
+        },
+    ),
+);
