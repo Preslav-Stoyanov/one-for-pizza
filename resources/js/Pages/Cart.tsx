8cab836e8dd592d09cf1bcf26171360ccae60917
preslavstoyanov <preslav.stoyanov@abv.bg>
2024-09-20 16:50:17 +0300

    empty cart design

--- resources/js/Pages/Cart.tsx
+++ resources/js/Pages/Cart.tsx
@@ -20,18 +20,33 @@ export default function Cart() {
             <Head title="Количка" />
             <Navbar />
             <div className="min-h-screen bg-amber-50">
-                <div className="flex h-full w-full flex-col items-center justify-between gap-16 py-4 sm:p-8 md:p-12 xl:flex-row xl:gap-48 2xl:gap-0">
-                    <div className="w-full grow xs:w-auto 2xl:w-1/2 2xl:grow-0">
-                        <Ribbon className="mx-auto mb-3">Количка</Ribbon>
-                        <CartTable />
+                {pizzas.length > 0 ? (
+                    <>
+                        <div className="flex h-full w-full flex-col items-center justify-between gap-16 py-4 sm:p-8 md:p-12 xl:flex-row xl:gap-48 2xl:gap-0">
+                            <div className="w-full grow xs:w-auto 2xl:w-1/2 2xl:grow-0">
+                                <Ribbon className="mx-auto mb-3">
+                                    Количка
+                                </Ribbon>
+                                <CartTable />
+                            </div>
+                            <OrderDetailsForm
+                                setCheckoutData={setCheckoutData}
+                            />
+                        </div>
+                        {checkoutData?.clientSecret && (
+                            <Checkout
+                                clientSecret={checkoutData.clientSecret}
+                                orderUuid={checkoutData.orderUuid}
+                            />
+                        )}
+                    </>
+                ) : (
+                    <div className="flex flex-col items-center gap-4 p-4">
+                        <Ribbon>Количка</Ribbon>
+                        <div className="w-full max-w-screen-md rounded bg-amber-100/50 py-4 text-center shadow-md">
+                            Количката ви е празна
+                        </div>
                     </div>
-                    <OrderDetailsForm setCheckoutData={setCheckoutData} />
-                </div>
-                {pizzas.length > 0 && checkoutData?.clientSecret && (
-                    <Checkout
-                        clientSecret={checkoutData.clientSecret}
-                        orderUuid={checkoutData.orderUuid}
-                    />
                 )}
             </div>
         </>
